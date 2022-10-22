f0fe4ec212b81480fc10a5be32febca0a93cc511
weiyee <weiyee731@gmail.com>
2022-10-22 15:58:32 +0800

    stock bug amendment

--- src/pages/Stock/WarehouseStockManagement.js
+++ src/pages/Stock/WarehouseStockManagement.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { GitAction } from "../../store/action/gitAction";
-import TableCell from '@mui/material/TableCell';
-import TableComponents from "../../components/TableComponents/TableComponents"
-import SearchBar from "../../components/SearchBar/SearchBar"
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { withRouter } from "react-router";
-import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
 import { isArrayNotEmpty, isStringNullOrEmpty, isNumber } from "../../tools/Helpers";
-import { Paper, TextField, Typography } from "@mui/material"
-import { toast, Flip } from "react-toastify"
+import { TextField, Typography } from "@mui/material"
+import { toast } from "react-toastify"
 import Button from '@mui/material/Button';
 import Barcode from 'react-barcode';
-
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import ReactToPrint, { PrintContextConsumer } from 'react-to-print';
-import PrintIcon from '@mui/icons-material/Print';
 import moment from 'moment';
-import { getDate } from "date-fns";
-import { IfObservable } from "rxjs/observable/IfObservable";
 
 function mapStateToProps(state) {
     return {
@@ -55,7 +47,6 @@ const INITIAL_STATE = {
     currentVolume: 65.5,
     isSubmitAdd: false,
     isSubmitDelete: false,
-    isSubmitData: [],
 
     stockData: [{
         TrackingNumber: "",
@@ -65,7 +56,6 @@ const INITIAL_STATE = {
         UserCode: "",
         isUserCodeError: false,
         UserData: "",
-        isUserDataError: false,
         Item: "",
         isItemError: false,
         Quantity: 1,
@@ -81,14 +71,11 @@ const INITIAL_STATE = {
         ProductDeep: "",
         isProductDeepError: false,
         Remark: "",
-        isRemarkError: false,
-        areaCode: "KU",
+        areaCode: "",
         createdDate: "",
         StockID: "",
     }],
-
 }
-
 
 class WarehouseStock extends Component {
     constructor(props) {
@@ -102,13 +89,15 @@ class WarehouseStock extends Component {
     componentDidMount() { }
 
     componentDidUpdate(prevProps, prevState) {
+        let listing = this.props.inventoryStock[0]
+        let userListing = this.props.userData[0]
 
         if (this.state.isCheckUser === true && this.props.userData.length > 0) {
-            let listing = this.props.userData[0]
+
             let arr = this.state.stockData
             if (this.props.userData[0].ReturnVal !== 0) {
-                arr[0].UserData = listing.Username
-                arr[0].areaCode = this.verifyAreaCode(listing.UserAreaID)
+                arr[0].UserData = userListing.Username
+                arr[0].areaCode = this.verifyAreaCode(userListing.UserAreaID)
             } else {
                 arr[0].UserData = ""
                 arr[0].areaCode = ""
@@ -118,11 +107,8 @@ class WarehouseStock extends Component {
             this.props.ClearUserCodeData()
         }
 
-        console.log("sdsadadad", this.props)
-
         if (this.props.inventoryStock.length > 0 && this.props.inventoryStock[0].ReturnVal !== 0 && this.state.isCheckDatabase === true) {
             let arr = this.state.stockData
-            let listing = this.props.inventoryStock[0]
             if (this.props.inventoryStock[0].ReturnVal !== 0) {
                 arr[0].CourierID = listing.CourierID
                 arr[0].UserCode = listing.UserCode
@@ -141,25 +127,23 @@ class WarehouseStock extends Component {
             this.setState({ isCheckDatabase: false, stockData: arr })
         }
         else {
-            console.log("componentDidUpdatecomponentDidUpdate", this.props.inventoryStock)
-            console.log("componentDidUpdatecomponentDidUpdate this.state.isCheckDatabase", this.state.isCheckDatabase)
-            if (this.props.inventoryStock.length > 0 && this.props.inventoryStock[0].ReturnVal === 0 && this.state.isCheckDatabase === true) {
-                // console.log("HEREEEE")
-                this.props.ClearInventoryStock()
-                this.setState({ StockID: "" })
-                // this.setInital()
-            }
+            if (this.props.inventoryStock.length > 0 && this.props.inventoryStock[0].ReturnVal === 0 && this.state.stockData[0].StockID !== "")
+                this.setInital(this.state.stockData[0].TrackingNumber)
         }
 
-        if (this.props.inventoryStockAction.length > 0 && this.state.isSubmitAdd == true) {
+        if (this.props.inventoryStockAction.length > 0 && this.state.isSubmitAdd === true) {
             if (this.props.inventoryStockAction[0].ReturnVal === 1) {
-                if (this.state.isSubmitDelete === true)
+                if (this.state.isSubmitDelete === true) {
                     toast.success("已成功删除")
-                else
-                    toast.success("已成功入库")
+                    window.location.reload(false)
+                }
+                else {
+                    if (this.state.stockData[0].StockID === "")
+                        toast.success("已成功入库")
+                    else
+                        toast.success("已更新成功")
+                }
 
-                this.setState({ TrackingNumber: "" })
-                this.setInital()
             } else {
                 if (this.state.isSubmitDelete === true)
                     toast.error("包裹未删除成功, 请联系系统管理")
@@ -167,17 +151,18 @@ class WarehouseStock extends Component {
                     toast.error("包裹未入库成功，请联系系统管理")
             }
             this.props.ClearInventoryAction()
+            this.setState({ isSubmitAdd: false, isSubmitDelete: false })
         }
     }
 
-    setInital() {
+    setInital(data) {
         this.setState({
             stockData: [{
-                TrackingNumber: "",
+                TrackingNumber: data,
                 isTrackingError: false, CourierID: "", isCourierError: false, UserCode: "", isUserCodeError: false,
-                UserData: "", isUserDataError: false, Item: "", isItemError: false, Quantity: 1, isQuantityError: false, ProductWeight: "", isProductWeightError: false,
+                UserData: "", Item: "", isItemError: false, Quantity: 1, isQuantityError: false, ProductWeight: "", isProductWeightError: false,
                 ProductVolumetricWeight: "", isProductVolumetricWeightError: false, ProductHeight: "", isProductHeightError: false, ProductWidth: "",
-                isProductWidthError: false, ProductDeep: "", isProductDeepError: false, Remark: "", isRemarkError: false, areaCode: "KU", createdDate: "", StockID: "",
+                isProductWidthError: false, ProductDeep: "", isProductDeepError: false, Remark: "", areaCode: "", createdDate: "", StockID: "",
             }]
         })
     }
@@ -210,7 +195,6 @@ class WarehouseStock extends Component {
                     arr.isUserCodeError = true
                 arr.UserCode = data
 
-                console.log("dsadadas", data)
                 if (data.length === 4 && this.state.isCheckUser === false) {
                     this.props.CallViewProfileByUserCode({ UserCode: arr.UserCode })
                     this.setState({ isCheckUser: true })
@@ -260,9 +244,6 @@ class WarehouseStock extends Component {
                 break;
 
             case "备注":
-                arr.isRemarkError = false
-                if (isStringNullOrEmpty(data))
-                    arr.isRemarkError = true
                 arr.Remark = data
                 break;
 
@@ -278,10 +259,10 @@ class WarehouseStock extends Component {
         let error = false
         if (listing.isTrackingError === false && listing.isCourierError === false && listing.isUserCodeError === false && listing.isItemError === false &&
             listing.isQuantityError === false && listing.isProductWeightError === false && listing.isProductHeightError === false &&
-            listing.isProductWidthError === false && listing.isProductDeepError === false && listing.isRemarkError === false) {
+            listing.isProductWidthError === false && listing.isProductDeepError === false) {
             if (listing.TrackingNumber !== "" && listing.CourierID !== "" && listing.UserCode !== "" && listing.Item !== "" &&
                 listing.Quantity !== "" && listing.ProductWeight !== "" && listing.ProductHeight !== "" &&
-                listing.ProductWidth !== "" && listing.ProductDeep !== "") {
+                listing.ProductWidth !== "" && listing.ProductDeep !== "" && listing.UserData !== "") {
                 error = false
             }
             else {
@@ -308,8 +289,6 @@ class WarehouseStock extends Component {
 
 
     createObject = () => {
-        console.log("sdsadadada", this.state.stockData[0])
-
         let listing = this.state.stockData[0]
         let UserCode = ""
         let TrackingNumber = ""
@@ -320,8 +299,6 @@ class WarehouseStock extends Component {
         let CourierID = ""
         let Item = ""
         let Remark = ""
-        let data = []
-
         let areaCode = ""
         let createdDate = ""
 
@@ -352,21 +329,6 @@ class WarehouseStock extends Component {
                 areaCode += ",";
                 createdDate += ",";
             }
-
-            data.push({
-                UserCode: listing.UserCode,
-                TrackingNumber: listing.Quantity > 1 ? listing.TrackingNumber.replace(/ /g, '') + "00" + parseInt(index + 1) : listing.TrackingNumber.replace(/ /g, ''),
-                ProductWeight: (isStringNullOrEmpty(listing.ProductWeight)) ? "0" : listing.ProductWeight,
-                ProductHeight: (isStringNullOrEmpty(listing.ProductHeight)) ? "0" : listing.ProductHeight,
-                ProductDeep: (isStringNullOrEmpty(listing.ProductDeep)) ? "0" : listing.ProductDeep,
-                ProductWidth: (isStringNullOrEmpty(listing.ProductWidth)) ? "0" : listing.ProductWidth,
-                CourierID: (isStringNullOrEmpty(listing.CourierID)) ? "0" : listing.CourierID,
-                Item: (isStringNullOrEmpty(listing.Item)) ? "-" : listing.Item.replace(/ /g, ''),
-                Remark: (isStringNullOrEmpty(listing.Remark)) ? "-" : listing.Remark.replace(/ /g, ''),
-                areaCode: (isStringNullOrEmpty(listing.areaCode)) ? "-" : listing.areaCode,
-                createdDate: (isStringNullOrEmpty(listing.createdDate)) ? "-" : listing.createdDate,
-                UserData: (isStringNullOrEmpty(listing.UserData)) ? "-" : listing.UserData,
-            })
         }
         let Obj = {
             UserCode: UserCode,
@@ -382,7 +344,6 @@ class WarehouseStock extends Component {
             createdDate: createdDate,
             ModifyBy: JSON.parse(localStorage.getItem("loginUser"))[0].UserID === null ? 1 : JSON.parse(localStorage.getItem("loginUser"))[0].UserID
         }
-        this.setState({ isSubmitData: data })
         return Obj
     }
 
@@ -463,6 +424,7 @@ class WarehouseStock extends Component {
                                 type="number"
                                 required
                                 size="small"
+                                disabled={stockData[0].StockID !== "" ? true : false}
                                 onChange={(e) => this.handleChange(e.target.value, "数量")}
                             />
                             {stockData[0].isQuantityError && <div><label style={{ color: "red", fontSize: "10pt" }}>请输入对的整数</label></div>}
@@ -510,7 +472,6 @@ class WarehouseStock extends Component {
 
 
         const handleButton = (type) => {
-            console.log("handleButton", type)
             switch (type) {
                 case "Save":
                     if (!this.verifyError()) {
@@ -546,6 +507,7 @@ class WarehouseStock extends Component {
                     } else {
                         toast.error("未入库单号不可删除")
                     }
+                    break;
 
                 default:
                     break;
@@ -557,11 +519,11 @@ class WarehouseStock extends Component {
             return (
                 data.length > 0 && data.map((x) => {
                     return (
-                        <div className="row" style={{ padding: "15pt" }} onClick={() => handleButton(x.type)} key={x.item} >
+                        <div className="row" style={{ padding: "5pt 20pt 15pt" }} onClick={() => handleButton(x.type)} key={x.item} >
                             {
                                 x.type === "Delete" || x.type === "Save" ?
                                     <Button style={{
-                                        paddingTop: "40pt", paddingBottom: "40pt", borderRadius: "20pt", color: "white", fontWeight: "bold", fontSize: "20pt",
+                                        paddingTop: "30pt", paddingBottom: "30pt", borderRadius: "20pt", color: "white", fontWeight: "bold", fontSize: "20pt",
                                         backgroundColor: this.verifyError() ? "grey" : "#0362fc"
                                     }} disabled={
                                         x.type === "Delete" ? this.props.inventoryStock.length > 0 && this.props.inventoryStock[0].ReturnVal === 0 ? true : false :
@@ -576,7 +538,7 @@ class WarehouseStock extends Component {
                                         trigger={(e) => {
                                             return (
                                                 <Button style={{
-                                                    paddingTop: "40pt", paddingBottom: "40pt", borderRadius: "20pt", color: "white", fontWeight: "bold", fontSize: "20pt",
+                                                    paddingTop: "30pt", paddingBottom: "30pt", borderRadius: "20pt", color: "white", fontWeight: "bold", fontSize: "20pt",
                                                     backgroundColor: this.verifyError() ? "grey" : "#0362fc"
                                                 }} disabled={this.verifyError() ? true : false}
                                                 >
@@ -584,6 +546,7 @@ class WarehouseStock extends Component {
                                                 </Button>
                                             );
                                         }}
+                                        onAfterPrint={() => { window.location.reload(false) }}
                                         content={() => this.componentRef}
                                     />
                             }
@@ -595,9 +558,10 @@ class WarehouseStock extends Component {
         }
 
         const renderPrintListing = () => {
-            let listing = this.state.stockData[0]
             let data = []
-            if (listing.length > 0) {
+            let listing = ""
+            if (this.state.stockData.length > 0) {
+                listing = this.state.stockData[0]
                 for (let index = 0; index < listing.Quantity; index++) {
                     data.push({
                         UserCode: listing.UserCode,
@@ -614,6 +578,7 @@ class WarehouseStock extends Component {
                     })
                 }
             }
+
             return (
                 data.length > 0 && data.map((x) => {
                     return (
@@ -623,14 +588,13 @@ class WarehouseStock extends Component {
                                 <Barcode value={x.TrackingNumber} height='80pt' />
                                 <div className="row" style={{ textAlign: "left" }}>
                                     <Typography style={{ fontWeight: "600", fontSize: "8pt", color: "#253949", letterSpacing: 1 }}>会员： {x.UserCode}</Typography>
-                                    <Typography style={{ fontWeight: "600", fontSize: "8pt", color: "#253949", letterSpacing: 1 }}>名称： {x.UserData}</Typography>
+                                    <Typography style={{ fontWeight: "600", fontSize: "8pt", color: "#253949", letterSpacing: 1 }}>称号： {x.UserData}</Typography>
                                     <Typography style={{ fontWeight: "600", fontSize: "8pt", color: "#253949", letterSpacing: 1 }}>入库：{moment(new Date()).format('DD-MM-YYYY, hh:mm:ss')}</Typography>
                                 </div>
                             </div>
                         </div>
                     )
                 })
-
             )
         }
 
@@ -649,7 +613,7 @@ class WarehouseStock extends Component {
                                         {listingLayout("快递单号", x.TrackingNumber, x.isTrackingError, "text")}
                                         {listingLayout("快递公司", x.CourierID, x.isCourierError, "list")}
                                         {listingLayout("会员号", x.UserCode, x.isUserCodeError, "text")}
-                                        {listingLayout("会员信息", x.UserData, x.isUserDataError, "text")}
+                                        {listingLayout("会员信息", x.UserData, "", "text")}
                                         {listingLayout("货物信息", x.Item, x.isItemError, "text")}
 
                                         <hr size="5" style={{ marginTop: "20pt" }} />
@@ -682,14 +646,12 @@ class WarehouseStock extends Component {
                                                 </div>
                                             </div>
                                         </div>
-                                        {listingLayout("备注", x.Remark, x.isRemarkError, "text")}
+                                        {listingLayout("备注", x.Remark, "", "text")}
                                     </>
                                 )
                             })
                         }
-
                     </div>
-                    {console.log("statstatsasa", this.state)}
 
                     <div className="col-xl-3 col-lg-3 col-md-3 col-sm-5 col-xs-12" >
                         <div className="row" style={{ textAlign: "center" }}>
@@ -702,7 +664,6 @@ class WarehouseStock extends Component {
                             { title: "删除", type: "Delete" }
                         ])}
                     </div>
-                    {console.log("dsdsadsadad", this.state.isSubmitData)}
 
                     <div style={{ display: "none" }} >
                         <div ref={(el) => (this.componentRef = el)}>
